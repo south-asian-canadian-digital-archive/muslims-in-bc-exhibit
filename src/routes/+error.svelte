9f466421251af276d1f2b7565075fdab97fa1b6f
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-04-19 11:41:45 -0700

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+{#if $page.status === 404}
+  <div class="h-screen w-screen flex items-center justify-center text-h1">
+    Page not found
+  </div>
+{:else}
+  <div class="h-screen w-screen flex items-center justify-center text-h1">
+    {$page.status} : {$page.error?.message}
+  </div>
+{/if}
