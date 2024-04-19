f4d5449b2637bc280af5fbf6244d44ae631e3965
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-04-19 13:31:40 -0700

    feat: init about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<main class="py-20">
+
+
+
+  <section class="bg-secondary-yellow flex flex-col pt-24">
+    <div class="flex flex-col px-32">
+      <h2 class="text-h6 font-bold border-b-2 border-secondar-teal w-fit">
+        We thank our Financial Supporters
+      </h2>
+      <div class="flex py-24 justify-between">
+        <img src="/BlackResearchGraduateStudies.png" class="h-24" alt="" />
+        <img src="/UFV_SASI_logo.png" alt="" class="h-24" />
+        <img src="/Sacda-logo.svg" alt="" class="h-18" />
+      </div>
+    </div>
+
+    <div class="bg-[url('/pattern.svg')] min-h-32 w-full bg-repeat-x">
+      &nbsp;
+    </div>
+  </section>
+
+
+
+</main>
