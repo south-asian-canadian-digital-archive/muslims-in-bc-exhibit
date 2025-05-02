86bce6b12a0d0f5941c49aec7436ea3e62404177
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-05-02 12:27:18 -0700

    feat: better visibility

--- src/lib/components/HistoryNavigation.svelte
+++ src/lib/components/HistoryNavigation.svelte
@@ -30,15 +30,17 @@
   {#if prevUrl !== null}
     <a
       href={prevUrl}
-      class="pointer-events-auto fixed left-0 md:top-1/2 md:-translate-y-1/2 top-24"
+      class="pointer-events-auto fixed left-0 md:top-1/2 md:-translate-y-1/2 top-24 !no-underline"
       aria-label="Previous section"
     >
-      <Button 
-        variant="outline" 
-        class="shadow-md md:w-1 md:h-fit w-fit h-8 rounded-l-none bg-secondary-yellow md:py-3 flex items-center"
+      <Button
+        variant="outline"
+        class="shadow-md  md:h-fit w-fit h-8 rounded-l-none bg-secondary-yellow hover:bg-secondary-yellow border border-transparent hover:border-primary-blue md:py-3 flex items-center transition-all duration-100"
       >
-        <span class="md:whitespace-normal md:my-2 uppercase md:leading-4 md:rotate-0 md:block flex items-center">
-          <Icon.ArrowLeft class="h-4 w-4 md:hidden mr-1" />
+        <span
+          class="md:whitespace-normal md:my-2 uppercase md:leading-4 md:rotate-0 flex items-center"
+        >
+          <Icon.ArrowLeft class="h-4 w-4 mr-1" />
           back
         </span>
       </Button>
@@ -48,16 +50,18 @@
   {#if nextUrl !== null}
     <a
       href={nextUrl}
-      class="pointer-events-auto fixed right-0 md:top-1/2 md:-translate-y-1/2 top-24"
+      class="pointer-events-auto fixed right-0 md:top-1/2 md:-translate-y-1/2 top-24 !no-underline"
       aria-label="Next section"
     >
-      <Button 
-        variant="outline" 
-        class="md:w-1 md:h-fit w-fit h-8 shadow-md rounded-r-none bg-secondary-yellow md:py-3 flex items-center"
+      <Button
+        variant="outline"
+        class="md:h-fit w-fit h-8 shadow-md rounded-r-none bg-secondary-yellow hover:bg-secondary-yellow border border-transparent hover:border-primary-blue md:py-3 flex items-center transition-all duration-100"
       >
-        <span class="md:my-2 md:whitespace-normal uppercase md:leading-4 flex items-center">
+        <span
+          class="md:my-2 md:whitespace-normal uppercase md:leading-4 flex items-center"
+        >
           Next
-          <Icon.ArrowRight class="h-4 w-4 md:hidden ml-1" />
+          <Icon.ArrowRight class="h-4 w-4  ml-1" />
         </span>
       </Button>
     </a>
