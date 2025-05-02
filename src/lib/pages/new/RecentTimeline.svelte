57a7a8b1abfce6e2d3e1c760e3447e43df5b49c4
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-05-02 12:36:56 -0700

    fix: moved the layout of recent timeline around a bit

--- src/lib/pages/new/RecentTimeline.svelte
+++ src/lib/pages/new/RecentTimeline.svelte
@@ -1,33 +1,35 @@
 <main class="py-32 mx-auto flex flex-col gap-10">
-  <h1 class="px-4 lg:px-0 page-title">
-    Canada and South Asian Canadian Muslims after 9/11
-  </h1>
-  <div class="desc-text">
-    The attacks on the twin towers on the 11th of September 2011, and America’s
-    response to them, forever changed the way that Muslims were perceived
-    globally. Scholars like Edward Said have pointed out that Muslims came to be
-    seen as a “flattened” category where the diversity of cultural and religious
-    practices was erased, particularly in the US. Canada’s proximity to the US
-    meant that it was seen as central to the response to 9/11 even being
-    specifically mentioned in the US Patriot Act under the section “Protecting
-    the Northern Border.” Scholars have argued that Canada’s response to 9/11
-    was affected by the need to appease the US so as to keep people and good
-    flowing across the border. In 2001However, the repercussions of these
-    policies was an increase in racial profiling, particularly of Muslim men,
-    who in some very high profile cases became the victims of inaccurate
-    information sharing between Canada and the US resulting in the torture of
-    these Canadian citizens. The Canadian government subsequently conducted a
-    report and apologized for these. Nonetheless, the antiterrorism laws,
-    increased border controls and deeply problematic representations of Muslims
-    as terrorists after 9/11 has continued to impact all groups of Canadian
-    Muslims.
-  </div>
+  <h1 class="px-4 lg:px-0 page-title">Recent Timeline</h1>
 
   <div class="flex flex-col gap-16 *:w-full *:grid *:grid-cols-12 history">
     <section>
-      <h2>Recent Timeline</h2>
+      <h2>Canada and South Asian Canadian Muslims after 9/11</h2>
+      <div class="">
+        The attacks on the twin towers on the 11th of September 2011, and
+        America’s response to them, forever changed the way that Muslims were
+        perceived globally. Scholars like Edward Said have pointed out that
+        Muslims came to be seen as a “flattened” category where the diversity of
+        cultural and religious practices was erased, particularly in the US.
+        Canada’s proximity to the US meant that it was seen as central to the
+        response to 9/11 even being specifically mentioned in the US Patriot Act
+        under the section “Protecting the Northern Border.” Scholars have argued
+        that Canada’s response to 9/11 was affected by the need to appease the
+        US so as to keep people and good flowing across the border. In
+        2001However, the repercussions of these policies was an increase in
+        racial profiling, particularly of Muslim men, who in some very high
+        profile cases became the victims of inaccurate information sharing
+        between Canada and the US resulting in the torture of these Canadian
+        citizens. The Canadian government subsequently conducted a report and
+        apologized for these. Nonetheless, the antiterrorism laws, increased
+        border controls and deeply problematic representations of Muslims as
+        terrorists after 9/11 has continued to impact all groups of Canadian
+        Muslims.
+      </div>
+    </section>
+
+    <section>
       <div class="!border-0 !mx-0 !p-0">
-        <p>
+        <p class="mb-6">
           This interactive timeline showcases key events affecting Muslims in
           British Columbia after 9/11. Navigate through the timeline by clicking
           the arrows on either side or by scrolling horizontally. Each slide
