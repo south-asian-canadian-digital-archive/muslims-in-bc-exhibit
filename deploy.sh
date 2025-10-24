c504e957da1c61e73fa1a1c670575c91684390de
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-10-24 15:58:27 -0700

    feat: more robust deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
 pnpm build
 
 cd build
@@ -7,3 +10,5 @@ git add -A
 git commit -m 'deploy'
 
 git push -f git@github.com:south-asian-canadian-digital-archive/muslims-in-bc-exhibit.git main:build
+
+echo "✅ Deployment successful!"
