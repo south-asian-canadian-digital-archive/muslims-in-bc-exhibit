e331cba5f158cb143c22ad2e6b317ea7582c3197
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-10-24 13:18:08 -0700

    fix: updated farida farook narative

--- src/lib/content/interviews.content.ts
+++ src/lib/content/interviews.content.ts
@@ -218,7 +218,7 @@ export const interviews: Interview[] = [
     interviewDate: "2025-01-12",
     interviewer: "Thamilini Jothilingam",
     contributors: ["Sadhik Preet Singh"],
-    narrativePdfUrl: 'https://drive.google.com/file/d/1geo0CvXq9bB4urPFdtbUZt0cFaqyic9s/view?usp=sharing',
+    narrativePdfUrl: 'https://drive.google.com/file/d/1geo0CvXq9bB4urPFdtbUZt0cFaqyic9s/view?usp=drive_link',
   },
 
   // {
